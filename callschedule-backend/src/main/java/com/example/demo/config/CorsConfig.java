c6e6a6209e9575f786aeb7440acbbc24e517f2a7
soham <sohamkadam16102004@gmail.com>
2025-11-27 11:48:51 +0530

    commiting with build

--- callschedule-backend/src/main/java/com/example/demo/config/CorsConfig.java
+++ callschedule-backend/src/main/java/com/example/demo/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://investza.in", "https://www.investza.in")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(false);
--- my-app/src/api/flowApi.js
+++ my-app/src/api/flowApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:8080/api";
+const API = "/api";
 
 export const startFlow = (fullName) => axios.post(`${API}/flow/start`, { fullName });
 
